b4ed08cf1128f9da00ddb18a24ad5202bfa400fb
Ajey Awasthi <115537579+dropps07@users.noreply.github.com>
2026-05-22 02:37:01 +0530

    cycle detect
    Fixed comments and improved variable naming in the duplicate number finding methods.

--- leetcode/code/FindDuplicateNumber.java
+++ leetcode/code/FindDuplicateNumber.java
@@ -1,11 +1,11 @@
 package leetcode.code;
-
+//#approach 1 
 class Solution{
     public int FindDuplicateNumber(int[] nums){
         int slow=0;
         int fast =0;
         do{
-            slow= nums[slow];  //floyd's repitetion check.
+            slow= nums[slow];  //floyd's repetition check.
             fast = nums[nums[fast]];
         }while(slow!=fast);
 
@@ -15,5 +15,28 @@ class Solution{
             fast = nums[fast];
         }
         return slow;
+    }
+}
+
+//#approach 2
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int left=0;
+        int rigth=nums.length;
+        while(left< rigth){
+            int mid= left + (rigth-left)/2;
+            int c=0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]<=mid){
+                    c++;
+                }
+            }
+            if(c<=mid){
+                left = mid+1;
+            }else{
+                rigth=mid;
+            }
+        }
+        return left;
     }
 }
