1462fb4a566e6b6bda13cb95043cf3177430232a
Ajey Awasthi <dropps07@Ajeys-MacBook-Air.local>
2026-05-25 01:06:13 +0530

    more files

---
+++ Code/day2/Basics.java
@@ -0,0 +1,21 @@
+package Code.day2;
+import java.util.*;
+
+public class Basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if(true){
+            System.out.println("True");
+        }
+        //while loop
+        int c=0;
+        while(c!=5){
+            System.out.println(c);
+            c++;
+        }
+        //for loop
+        for(int i=0;i<=5;i++){
+            System.out.println(i);
+        }
+    }
+}
